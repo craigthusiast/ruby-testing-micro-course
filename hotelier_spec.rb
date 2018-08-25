b97d3c7acd5ced6473297e685d7cc906de23d0fd
craigthusiast <craig.desbrisay@gmail.com>
2018-08-25 15:03:09 -0400

    lesson 1-b

--- hotelier_spec.rb
+++ hotelier_spec.rb
@@ -11,6 +11,10 @@ class Hotel
     @guests << guest_name
   end
 
+  def check_out_guest(guest_name)
+    @guests.delete(guest_name)
+  end
+
 end
 
 describe Hotel do
@@ -18,5 +22,14 @@ describe Hotel do
     hotel = Hotel.new
     hotel.check_in_guest("Craig DesBrisay")
     expect(hotel.guests).to include 'Craig DesBrisay'
+  end
+
+  it 'can check a guest out' do
+    hotel = Hotel.new
+    hotel.check_in_guest("Craig DesBrisay")
+    hotel.check_in_guest("Christie Longhurst")
+    hotel.check_out_guest("Craig DesBrisay")
+    expect(hotel.guests).not_to include 'Craig DesBrisay'
+    $stderr.puts hotel.guests
   end
 end

77c254ec2469460131fc5885f90a78ea5d56a019
craigthusiast <craig.desbrisay@gmail.com>
2018-08-25 11:18:14 -0400

    lesson 1

--- hotelier_spec.rb
+++ hotelier_spec.rb
@@ -1,1 +1,22 @@
-empty 
+require 'rspec'
+
+class Hotel
+  attr_accessor :guests
+
+  def initialize
+    @guests = []
+  end
+
+  def check_in_guest(guest_name)
+    @guests << guest_name
+  end
+
+end
+
+describe Hotel do
+  it 'can check in a guest' do
+    hotel = Hotel.new
+    hotel.check_in_guest("Craig DesBrisay")
+    expect(hotel.guests).to include 'Craig DesBrisay'
+  end
+end

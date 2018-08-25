465b743f863c5ed3b30cee9e3d5441135008a1b5
craigthusiast <craig.desbrisay@gmail.com>
2018-08-25 15:24:53 -0400

    lesson 2

--- hotelier_spec.rb
+++ hotelier_spec.rb
@@ -1,14 +1,16 @@
 require 'rspec'
 
 class Hotel
-  attr_accessor :guests
+  attr_accessor :guests, :rooms_occupied
 
   def initialize
     @guests = []
+    @rooms_occupied = []
   end
 
-  def check_in_guest(guest_name)
+  def check_in_guest(guest_name, room_number)
     @guests << guest_name
+    @rooms_occupied << room_number
   end
 
   def check_out_guest(guest_name)
@@ -20,16 +22,18 @@ end
 describe Hotel do
   it 'can check in a guest' do
     hotel = Hotel.new
-    hotel.check_in_guest("Craig DesBrisay")
+    hotel.check_in_guest("Craig DesBrisay", 212)
     expect(hotel.guests).to include 'Craig DesBrisay'
   end
 
   it 'can check a guest out' do
     hotel = Hotel.new
-    hotel.check_in_guest("Craig DesBrisay")
-    hotel.check_in_guest("Christie Longhurst")
+    hotel.check_in_guest("Craig DesBrisay", 212)
+    hotel.check_in_guest("Christie Longhurst", 213)
     hotel.check_out_guest("Craig DesBrisay")
     expect(hotel.guests).not_to include 'Craig DesBrisay'
+    expect(hotel.rooms_occupied).to include 212
     $stderr.puts hotel.guests
+    $stderr.puts hotel.rooms_occupied
   end
 end
